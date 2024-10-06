f9404649d7e346a6dabf2a78a857b93ee34f9f55
Sachin-8125 <devinewolf7@gmail.com>
2024-10-07 02:29:38 +0530

    Implemented basic crate management

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use std::fs::read_to_string;
+use chrono::{Local,Utc};
 
 fn main() {
-    let ans = read_from_file_sachin(String::from("a.txt"));
+    //get current date and time in utc
+    let now = Utc::now();
+    println!("Current date and time in UTC: {}",now);
+
+    //format date and time
+    let formatted = now.format("%Y-%m-%d %H:%M:%S");
+    println!("Formatted date and time: {}",formatted);
+
+    //get local time
+    let local = Local::now();
+    println!("Current date and time in local: {}",local);
 }
 
-fn read_from_file_sachin(file_path: String) -> Result<String, String>{
-    let result = read_to_string(file_path);
-    match result{
-        Ok(data) => Ok(data),
-        Err(err) => Err(String::from("File not found")),
-    }
-    }
+
+
