3124c5f7887f1f87abfc26b32d948f9adea4fb77
Sachin-8125 <devinewolf7@gmail.com>
2024-10-07 05:26:36 +0530

    Implemented vector using generics

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 fn main() {
-    let mut vec = Vec::new();
-    vec.push(1);
-    vec.push(2);
-    vec.push(3);
-    vec.push(4);
-    println!("{:?}",even_filter(vec));
+    let mut vec = vec![1,2,3,4];
+    let ans = even_filter(&vec);
+    println!("{:?}",ans);
 }
 
-fn even_filter(vec: Vec<i32>) -> Vec<i32>{
+fn even_filter(vec: &Vec<i32>) -> Vec<i32>{
     let mut new_vec = Vec::new();
     for val in vec{
         if val%2==0 {
-            new_vec.push(val);
+            new_vec.push(*val);
         }
     }
     return new_vec;
