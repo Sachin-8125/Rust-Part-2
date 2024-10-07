a9de3a1168e941660a9ae71348bc59d438df9508
Sachin-8125 <devinewolf7@gmail.com>
2024-10-07 06:13:03 +0530

    Implemented filter and map function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
-   let nums = vec![1,2,3,4,5];
-   let  iter = nums.into_iter();
+   let v1 = vec![1,2,3];
+   let ans = filter_and_map(v1);
+   println!("{:?}",ans);
+}
 
-   for val in iter{
-    print!("{} ", val);
-   }
+fn filter_and_map(v: Vec<i32>)->Vec<i32>{
+    let new_iter = v.iter().filter(|x|*x%2==1).map(|x|x+1);
+    let new_vec = new_iter.collect();
+    return new_vec;
 }
 
 
